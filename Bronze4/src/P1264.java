39819d31bfa264fe4e8ecbbfe059468da20fe84e
Rayne <pluvstream@gmail.com>
2025-04-12 23:41:10 +0900

    250412

---
+++ Bronze4/src/P1264.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P1264 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      String sentence = scanner.nextLine();
+      if (sentence.equals("#")) break;
+
+      String[] alphabet = sentence.split("");
+      int count = 0;
+      for (String s : alphabet) {
+        if ("AEIOUaeiou".contains(s)) count++;
+      }
+
+      System.out.println(count);
+    }
+  }
+}
