648adfd7f26aa6680340501a765d33c54c94311f
Rayne <pluvstream@gmail.com>
2025-04-14 17:09:52 +0900

    250414

---
+++ Bronze3/src/P10988.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P10988 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String word = scanner.nextLine();
+    String reverseWord = new StringBuffer(word).reverse().toString();
+
+    if (word.equals(reverseWord)) {
+      System.out.println("1");
+    } else {
+      System.out.println("0");
+    }
+  }
+}
