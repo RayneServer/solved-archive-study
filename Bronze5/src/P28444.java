fa277eabbaede58a3c355fc174a3a8d08f44d22b
Rayne <pluvstream@gmail.com>
2025-04-30 19:35:07 +0900

    250430

---
+++ Bronze5/src/P28444.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class P28444 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println((scanner.nextInt() * scanner.nextInt()) - (scanner.nextInt() * scanner.nextInt() * scanner.nextInt()));
+  }
+}
