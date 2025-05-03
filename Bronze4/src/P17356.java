f47567abb2ee2e9571ce2108a8e2077f857a6d5c
Rayne <pluvstream@gmail.com>
2025-05-03 23:53:28 +0900

    250503

---
+++ Bronze4/src/P17356.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P17356 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int wookPower = scanner.nextInt();
+    int jePower = scanner.nextInt();
+
+    double num_M = (double) (jePower - wookPower) / 400;
+    double result = 1 / (1 + Math.pow(10, num_M));
+
+    System.out.println(result);
+  }
+}
