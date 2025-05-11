3e7a40a2f571ebbec23bef6c37e268de5b253b07
Rayne <pluvstream@gmail.com>
2025-05-11 23:06:38 +0900

    250511

---
+++ Bronze4/src/P11943.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P11943 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int[] firstBucket = {scanner.nextInt(), scanner.nextInt()};
+    int[] secondBucket = {scanner.nextInt(), scanner.nextInt()};
+
+    System.out.println(Math.min(firstBucket[0] + secondBucket[1], firstBucket[1] + secondBucket[0]));
+  }
+}
