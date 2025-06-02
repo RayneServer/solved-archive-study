27e5fd31655d8b85a6f6ae176f0ee9fb7fa8286b
Rayne <pluvstream@gmail.com>
2025-06-02 17:54:09 +0900

    250602

---
+++ Bronze2/src/P1075.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P1075 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int numN = scanner.nextInt();
+    int numF = scanner.nextInt();
+
+    String result;
+    if ((numN / 100 * 100) % numF == 0) {
+      result = String.valueOf(numN / 100 * 100);
+    } else {
+      result = String.valueOf(((numN / 100 * 100) + numF) - (((numN / 100 * 100) + numF) % numF));
+    }
+
+    System.out.println(result.substring(result.length() - 2));
+  }
+}
