f3db092dac305403c2f2656f763d090289661476
Rayne <pluvstream@gmail.com>
2025-06-13 14:28:49 +0900

    250613

---
+++ Bronze3/src/P15894.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class P15894 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println((scanner.nextInt()) * (long) 4);
+  }
+}
