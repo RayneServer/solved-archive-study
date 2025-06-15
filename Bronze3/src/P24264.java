b8ad1af07421bfc2992c33c8e9a12ccd25b97112
Rayne <pluvstream@gmail.com>
2025-06-15 23:43:11 +0900

    250615

---
+++ Bronze3/src/P24264.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P24264 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    long numN = scanner.nextLong();
+    System.out.println(numN * numN);
+    System.out.println(2);
+  }
+}
