7170d5579209b9921510085e5257fc4777a27711
Rayne <pluvstream@gmail.com>
2025-06-19 14:23:24 +0900

    250619 + Achieve Gold Tier :)

---
+++ Silver3/src/P2346.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class P2346 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Deque<int[]> deque = new ArrayDeque<>();
+    StringBuilder stringBuilder;
+
+    int numbers = scanner.nextInt();
+    for (int i = 1; i <= numbers; i++) {
+      deque.offer(new int[] {i, scanner.nextInt()});
+    }
+
+    stringBuilder = new StringBuilder();
+    while (true) {
+      int[] item = deque.poll();
+      stringBuilder.append(item[0] + " ");
+      int move = item[1];
+
+      if (deque.isEmpty()) break;
+
+      if (move > 0) {
+        for (int i = 0; i < move - 1; i++) {
+          deque.offer(deque.poll());
+        }
+      } else {
+        for (int i = 0; i < (move * -1); i++) {
+          deque.offerFirst(deque.pollLast());
+        }
+      }
+    }
+
+    System.out.println(stringBuilder);
+  }
+}
---
+++ Silver4/src/P1269.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class P1269 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer stringTokenizer;
+    Set<String> groupA = new HashSet<>();
+    Set<String> groupB = new HashSet<>();
+
+    String[] settings = bufferedReader.readLine().split(" ");
+    int groupASize = Integer.parseInt(settings[0]);
+    int groupBSize = Integer.parseInt(settings[1]);
+
+    stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+    for (int i = 0; i < groupASize; i++) {
+      groupA.add(stringTokenizer.nextToken());
+    }
+
+    stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+    for (int i = 0; i < groupBSize; i++) {
+      groupB.add(stringTokenizer.nextToken());
+    }
+
+    Set<String> unionAB = new HashSet<>(groupA);
+    unionAB.addAll(groupB);
+
+    Set<String> retainAB = new HashSet<>(groupA);
+    retainAB.retainAll(groupB);
+
+    unionAB.removeAll(retainAB);
+    System.out.println(unionAB.size());
+  }
+}
