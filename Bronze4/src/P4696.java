41f186db5758a12e7cdd8bcf4c286bda8ed1fde1
Rayne <pluvstream@gmail.com>
2025-07-01 11:42:04 +0900

    250701

---
+++ Bronze4/src/P4696.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P4696 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      double number = scanner.nextDouble();
+      if (number == 0.0) break;
+
+      double sum = 1;
+      for (int i = 1; i <= 4; i++) {
+        sum += Math.pow(number, i);
+      }
+
+      System.out.printf("%.2f\n", sum);
+    }
+  }
+}
