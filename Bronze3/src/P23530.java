b512d542635f32b6a41be0bca4519235c7e2080b
Rayne <pluvstream@gmail.com>
2025-07-04 09:20:54 +0900

    250704

---
+++ Bronze3/src/P23530.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class P23530 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int testCase = scanner.nextInt();
+    for (int i = 0; i < testCase; i++) {
+      int numA = scanner.nextInt();
+      int numB = scanner.nextInt();
+
+      int numC = numA + numB;
+      for (int j = 1; j <= 50; j++) {
+        if (j != numC) {
+          System.out.println(j);
+          break;
+        }
+      }
+    }
+  }
+}
