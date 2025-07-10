f27a0ae8b1858bdd4006e070545bc6ab34440ed6
Rayne <pluvstream@gmail.com>
2025-07-10 22:04:52 +0900

    250710

---
+++ Bronze4/src/P33810.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P33810 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String myWord = "SciComLove";
+    String yourWord = scanner.nextLine();
+
+    int count = 0;
+    for (int i = 0; i < myWord.length(); i++) {
+      char myChar = myWord.charAt(i);
+      char yourChar = yourWord.charAt(i);
+
+      if (myChar != yourChar) count++;
+    }
+
+    System.out.println(count);
+  }
+}
