1427873bea047bec84e372d3c6f0955e51b3e0f6
Rayne <pluvstream@gmail.com>
2025-07-26 23:58:44 +0900

    250726

---
+++ Bronze5/src/P15680.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P15680 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int num = scanner.nextInt();
+
+    if (num == 1) {
+      System.out.println("Leading the Way to the Future");
+    }
+
+    if (num == 0) {
+      System.out.println("YONSEI");
+    }
+  }
+}
