0a44c376f4669f7bf89e7fca8264f3a7876ac249
Rayne <pluvstream@gmail.com>
2025-08-05 09:18:52 +0900

    250805

---
+++ Bronze3/src/P32046.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class P32046 {
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (true) {
+        int num = scanner.nextInt();
+        if (num == 0) break;
+
+        int[] snacks = new int[num];
+        for (int i = 0; i < snacks.length; i++) {
+          snacks[i] = scanner.nextInt();
+        }
+
+        System.out.println(calcPrice(snacks));
+      }
+    }
+  }
+
+  private static int calcPrice(int[] snacks) {
+    int sum = 0;
+    for (int snack : snacks) {
+      if (sum + snack <= 300) sum += snack;
+    }
+
+    return sum;
+  }
+}
